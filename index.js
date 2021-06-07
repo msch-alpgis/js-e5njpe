c5b82294c712e8bba0fc259c988b92b66463cea7
msch-alpgis <m.schenk@alpgis.ch>
2021-06-07 13:25:38 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import proj4 from 'proj4';
+import { register } from 'ol/proj/proj4';
+
+proj4.defs(
+  'EPSG:2056',
+  '+proj=somerc \
+    +lat_0=46.95240555555556 +lon_0=7.439583333333333 \
+    +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel \
+    +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs'
+);
+register(proj4);
+
+function fakeRequest(fileName) {
+  const request = new Request(`/assets/${fileName}`);
+  fetch(request)
+    .then(response => response.text())
+    .then(xml => {
+      console.log(xml);
+    });
+}
+
+document.getElementById('single').addEventListener('click', _ => {
+  fakeRequest('single.xml');
+});
